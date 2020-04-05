b14e9d2496b3bce7b1bcf991b9468f9ece2710b6
Hoang <Le>
2020-04-04 20:26:03 -0700

    Added weighted random variable, intergrated with DB

--- test.py
+++ test.py
@@ -1,58 +1,77 @@
 import subprocess
 import random
+import SpyfallDB as db
+import pandas as pd
 
-def removeAFK (guestList):  #Creates a list of people online
-    players = {}
-    for i in list(guestList.keys()):
-        if (guestList[i]["playing"] == True):
-                players[i] = guestList[i]
-                players[i]["Weight"] = 100
+def get_player_list():  #Creates a list of people online
+    guestList = db.get_all_players()
+    players = []
+    subprocess.call("cls", shell=True) 
+    for element in guestList:
+        Flag = True
+        while Flag:
+            active_player_query = input(f"Is {element['FirstName']} playing?  (Y/N)  ")
+            if active_player_query.lower() == "y":
+                Flag = False
+                element["SpyWeight"] = 100
+                element["Location"] = "Location"
+                element["SpyCount"] = 0
+                players.append(element)
+            elif active_player_query.lower() =="n":
+                Flag = False
+            else:
+                print("Please make a valid choice.")         
     return players
 
-def runGame(players):
-    subprocess.call("cls", shell=True)   
-    currentPlayers = players.copy()
-    spyIndex = random.choice(list(players.keys()))
-    players = setSpyWeight(players,spyIndex)
-    spy = currentPlayers.pop(spyIndex)
-    messagePlayers(spy, currentPlayers)
+def run_game(players):
+    #subprocess.call("cls", shell=True)   
+    players = set_location(players)
+    set_spy(players)
+    message_players(players)
 
-def setSpyWeight(players, spyIndex):
-    for i in list(players.keys()):
-        if (i != spyIndex):
-            players[i]["Weight"] = players[i]["Weight"] + random.randint(1,11)
+def set_location(players):
+    # also sets spy weight and increase spy count
+
+    for element in players:
+            element["Location"] = "Location"
+            element["SpyWeight"] = element["SpyWeight"] + random.randint(1,11)
     return players
+    
+def set_spy(players):
+    my_list = []
+    for i in players:
+        my_list = my_list + [str(i["UserId"])] * i["SpyWeight"]
+    spy_id = int(random.choice(my_list))
+    for i in players:
+        if i["UserId"] == spy_id:
+            i["Location"] = "SPY"
+            i["SpyWeight"] = 100
+            i["SpyCount"] += 1
 
-def messagePlayers (spy, players):
-    print (f"{spy['name']}, you are the spy!")
-    for i in list(players.keys()):
-        print(players[i]["name"] +", you are at this location")
+def message_players (players):
+    for element in players:
+        if (element["Location"]) != "SPY":
+            print(f"{element['FirstName']}, you are at this location: {element['Location']}")
+        else:
+            print (f"{element['FirstName']}, you are the spy!")
+    print(pd.DataFrame(players))
 
-guestList = {0:{"name": "Test", "carrier": "T-Mobile", "playing": False},
-    1:{"name": "Hoang Le", "carrier": "Verizon", "playing": True}, 
-    2:{"name": "Jon Luu", "carrier": "ATT", "playing": False},
-    3:{"name": "Work", "carrier": "Verizon", "playing": True},
-    4:{"name": "Jillian Luu", "carrier": "Verizon", "playing": True},
-    5:{"name": "Joannaly Tapang", "carrier": "Verizon", "playing": True}}
-
-players = removeAFK(guestList)
-
+players = get_player_list()
 flag=True
-
 while flag:
     print(""" 
-    **************************************************************************************
-    *                                                                                    *
-    *        1 - Start New Game                                                          *
-    *        2 - End Game                                                                *
-    *                                                                                    *
-    **************************************************************************************
+    ************************************
+    *                                  *
+    *        1 - Start New Game        *
+    *        2 - End Game              *
+    *                                  *
+    ************************************
     """)
     myCommand = input("What would you like to do: ")
     
     print(myCommand)
     if (myCommand == "1"):
-        runGame(players)            
+        run_game(players)            
     elif (myCommand == "2"):
         flag = False
     else:
