cff6ead0a56b0b18c69f2c39a8a169674adb7c14
Jonathan Luu <gotjukies@JLUU-MARTY.local>
2020-04-14 11:30:26 -0500

    Moved SpyfallPlayer to example.py

--- SpyfallDB.py
+++ SpyfallDB.py
@@ -27,40 +27,6 @@ class SpyfallDB:
         # params after self are for dealing with exceptions
         self.conn.close()    
         
-class SpyfallPlayer:
-    def __init__(self, playerid):
-        list = get_all_players()
-        player = [element for element in list if element['PlayerId'] == playerid]
-        self.player_id = playerid
-        self.fname = [element['FirstName'] for element in player][0]
-        self.lname = [element['LastName'] for element in player][0]
-        self.mobile_num = [element['MobileNum'] for element in player][0]
-        self.mobile_car = [element['MobileCarrier'] for element in player][0]
-        self.is_playing = None
-        self.is_spy = False
-        self.spy_weight = 100
-        
-    def set_is_playing(self, value):
-        if value.lower() == 'true':
-            with SpyfallDB() as db:
-                query = f"""UPDATE tblGameSession
-                        SET IsPlaying = %s
-                        WHERE PlayerId = %s"""
-                val = (1, self.player_id)
-                db.execute(query, val)
-            self.is_playing = True
-        elif value.lower() == 'false':
-            with SpyfallDB() as db:
-                query = f"""UPDATE tblGameSession
-                        SET IsPlaying = %s
-                        WHERE PlayerId = %s"""
-                val = (0, self.player_id)
-                db.execute(query, val)
-            self.is_playing = False
-        else:
-            return False
-        return True
-        
 def get_all_players(df=""):
     with SpyfallDB() as db:
         query = 'SELECT * FROM tblPlayer'
